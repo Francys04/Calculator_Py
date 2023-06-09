fede2f2a9a94f546b53a0da0699187650506939c
Francys04 <133686327+Francys04@users.noreply.github.com>
2023-06-09 19:51:07 +0300

    Update alarm-clock.py

--- alarm-clock.py
+++ alarm-clock.py
@@ -24,7 +24,7 @@ def alarm(seconds):
         print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
         
         
-    playsound("iphone_alarm.mp3")
+    playsound("iphone_alarm.mp3") # put the name of any sound file
     
 minutes = int(input("How many minutes to wait: "))
 seconds = int(input("How many seconds to wait: "))
